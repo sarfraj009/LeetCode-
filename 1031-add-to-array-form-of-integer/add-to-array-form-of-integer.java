6f21ccbae9119c3178733313440f69c1d47722eb
Sarfraj Ansari <sarfrajansari0127@gmail.com>
2026-08-08 23:48:42 +0530

    Time: 3 ms (74.29%) | Memory: 47.7 MB (67.73%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int p = num.length - 1;
+        int carry = 0;
+
+        while (p >= 0 || k > 0 || carry > 0) {
+
+            int numval = 0;
+
+            if (p >= 0) {
+                numval = num[p];
+            }
+
+            int d = k % 10;
+
+            int sum = numval + d + carry;
+
+            int digit = sum % 10;
+
+            carry = sum / 10;
+
+            ans.add(digit);
+
+            p--;
+            k = k / 10;
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
